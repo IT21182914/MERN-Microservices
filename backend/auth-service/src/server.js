aafe7f856c197553252419e5e2d61f63512a5cc6
Dilan Shanuka <dilanshanuka999@gmail.com>
2025-03-30 21:25:20 +0530

    Updates

--- backend/auth-service/src/server.js
+++ backend/auth-service/src/server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+//security middlewares
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 mins
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests, please try again later.",
+  })
+);
 app.use(cors());
 app.use(express.json());
 
